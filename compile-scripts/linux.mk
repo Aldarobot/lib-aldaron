75aa0aac04faa67562424f611620798120469722
jeron <deadbeef.jal@gmail.com>
2016-08-11 15:25:26 -0500

    Fixed SDL not building on Fedora.

--- compile-scripts/linux.mk
+++ compile-scripts/linux.mk
@@ -2,6 +2,6 @@ PLATFORM = Linux
 
 $(OBJ_SDL): $(SRC_SDL)
 	printf "[COMP] compiling SDL...\n"
-	cd $(SRC_SDL)/ && sh configure --prefix=`pwd`/usr_local/ && make -j 4 && make install
+	cd $(SRC_SDL)/ && sh configure --disable-video-wayland --prefix=`pwd`/usr_local/ && make -j 4 && make install
 	ld -r $(SRC_SDL)/build/.libs/*.o -o $(OBJ_SDL)
 	printf "[COMP] done!\n"

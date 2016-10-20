bcbdcad1a849903003c90336d26e11aeacd30cdd
jeron <deadbeef.jal@gmail.com>
2016-10-20 13:54:05 -0500

    Can now detect all platforms.

--- src/lib-aldaron/base/include/la.h
+++ src/lib-aldaron/base/include/la.h
@@ -11,14 +11,25 @@
 #if defined(__ANDROID__)
 	#define LA_PHONE
 	#define LA_ANDROID
-#elif defined(__IPHONEOS__)
+#elif defined(__IPHONEOS__) || defined(TARGET_OS_IPHONE) \
+ || defined(TARGET_IPHONE_SIMULATOR)
 	#define LA_PHONE
 	#define LA_PHONE_APPLE
 #else
 	#define LA_COMPUTER
-	#define LA_LINUX
-	#define LA_APPLE
-	//#define LA_WINDOWS
+	#if defined(__unix__)
+		#if defined(__APPLE__)
+			#define LA_APPLE
+		#else
+			#define LA_LINUX
+		#endif
+		#define LA_UNIX
+	// Any definiton used for windows
+	#elif defined(WIN32) || defined(_WIN32) || defined(_WIN64) \
+	 || defined (__WIN32__) || defined(__CYGWIN__) ||  defined(__MINGW32__)\
+	 || defined(__BORLANDC__)
+		#define LA_WINDOWS
+	#endif
 #endif
 
 //Determine Which Graphics Library to use.
@@ -29,10 +40,11 @@
 
 // Platform Capabilities.
 #if defined(LA_COMPUTER)
-	// All Linux Platforms
-	#define LA_GLTYPE LA_GLTYPE_SDL_ES2
-	// Windows
-	// #define LA_GLTYPE LA_GLTYPE_SDL_GL2
+	#if defined(LA_UNIX)
+		#define LA_GLTYPE LA_GLTYPE_SDL_ES2
+	#else
+		#define LA_GLTYPE LA_GLTYPE_SDL_GL2
+	#endif
 #elif defined(LA_PHONE)
 	#define LA_GLTYPE LA_GLTYPE_SDL_ES2
 #else
